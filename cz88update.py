b743e3d43977464ac6dd6a654cbd421e9fff0bb2
animalize <wjsszsh@sohu.com>
2015-08-07 15:07:18 +0800

    从纯真网络(cz88.net)更新qqwry.dat的小工具

---
+++ cz88update.py
@@ -0,0 +1,98 @@
+# coding=utf-8
+#
+# 用于从纯真网络(cz88.net)更新qqwry.dat
+# for Python 3.0+
+# 来自 https://github.com/animalize/qqwry-python3
+#
+# 用法：
+# from cz88update import updateQQwry
+# result = updateQQwry(filename)  #filename为要保存的文件名
+#
+# updateQQwry函数返回值
+# 正整数：表示已成功更新，为qqwry.dat的字节数
+# -1：下载copywrite.rar出错
+# -2：解析copywrite.rar出错
+# -3：下载qqwry.rar出错
+# -4：qqwry.rar文件大小不符合copywrite.rar的数据
+# -5：解压缩qqwry.rar出错
+# -6：保存到最终文件时出错
+
+import struct
+import urllib.request
+import zlib
+    
+def updateQQwry(filename):
+    def get_fetcher():
+        # no proxy
+        proxy = urllib.request.ProxyHandler({})
+        # opener
+        opener = urllib.request.build_opener(proxy)
+        
+        def open_url(url):
+            # request对象
+            req = urllib.request.Request(url)
+            ua = ('Mozilla/5.0 (Windows NT 6.1; rv:38.0)'
+                  ' Gecko/20100101 Firefox/38.0')
+            req.add_header('User-Agent', ua)
+            
+            try:
+                # r是HTTPResponse对象
+                r = opener.open(req, timeout=60)
+                return r.read()
+            except Exception as e:
+                return None
+        
+        return open_url
+    
+    fetcher = get_fetcher()
+    
+    # copywrite.rar
+    url = 'http://update.cz88.net/ip/copywrite.rar'
+    data = fetcher(url)
+    if not data:
+        return -1
+    
+    # extract infomation
+    if len(data) <= 24 or data[:4] != b'CZIP':
+        return -2
+    
+    version, unknown1, size, unknown2, key = \
+        struct.unpack_from('<IIIII', data, 4)
+    if unknown1 != 1:
+        return -2
+    
+    # qqwry.rar
+    url = 'http://update.cz88.net/ip/qqwry.rar'
+    data = fetcher(url)
+    
+    if not data:
+        return -3
+    
+    if size != len(data):
+        return -4
+    
+    # decrypt
+    head = bytearray(0x200)
+    for i in range(0x200):
+        key = (key * 0x805 + 1) & 0xff
+        head[i] = data[i] ^ key
+    data = head + data[0x200:]
+    
+    try:
+        data = zlib.decompress(data)
+    except:
+        return -5
+
+    try:
+        with open(filename, 'wb') as f:
+            f.write(data)
+        return len(data)
+    except:
+        return -6
+
+if __name__ == '__main__':
+    ret = updateQQwry('qqwry.dat')
+    if ret > 0:
+        print('成功更新qqwry.dat，%s字节' % format(ret, ','))
+    else:
+        print('更新失败，错误代码：%d' % ret)
